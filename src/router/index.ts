f4f9fadc8f04d407786aaa384491b9a709c3c983
zeng715734199 <715734199@qq.com>
2021-08-30 22:03:22 +0800

    添加了404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter)
 const  routes = [
@@ -21,6 +22,10 @@ const  routes = [
   {
     path: '/statistics',
     component: Statistics
+  },
+  {
+    path: '*',
+    component: NotFound
   }
 ]
 
