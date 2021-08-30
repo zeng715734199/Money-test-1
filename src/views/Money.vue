d9253000547b6f53cd5fff73a546a3864083c273
zeng715734199 <715734199@qq.com>
2021-08-31 00:15:27 +0800

    设置了money的导航

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+    Money.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -11,5 +14,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  flex-grow: 1;
+  overflow: auto;
+  border: 1px solid blue;
+}
 </style>
