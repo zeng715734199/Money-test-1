3ccab374c7e2cb1d5d9fff0bbabc053d02117ce3
zeng715734199 <715734199@qq.com>
2021-09-01 00:12:32 +0800

    添加了Icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,25 @@
 <template>
-<div class="nav">
-  <router-link to="/money">记账</router-link>
-  |
-  <router-link to="/labels">标签</router-link>
-  |
-  <router-link to="/statistics">统计</router-link>
-</div>
+  <div class="nav">
+    <Icon name="money"/>
+    <router-link to="/money">记账</router-link>
+    |
+    <Icon name="label"/>
+    <router-link to="/labels">标签</router-link>
+    |
+    <Icon name="statistics"/>
+    <router-link to="/statistics">统计</router-link>
+  </div>
 </template>
 
 <script lang="ts">
+
 export default {
   name: 'Nav'
 };
 </script>
 
 <style lang="scss" scoped>
-.nav{
+.nav {
   border: 1px solid red;
 }
 </style>
