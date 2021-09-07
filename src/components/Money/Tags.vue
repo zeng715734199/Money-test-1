4ba2a94cfc6cb7daad541eee0aada6878dee43bb
zeng715734199 <715734199@qq.com>
2021-09-07 19:28:43 +0800

    给layout传åprops

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -3,13 +3,12 @@
     <span v-for="tag in dataSource" :key="tag"
           @click="toggle(tag)"
           :class="{selected: selectedTags.indexOf(tag)>=0}">
-      <Icon :name="tag"/>
+<!--      <Icon :name="tag"/>-->
       {{ tag }}
     </span>
-<!--    <div  @click="create" class="new">-->
+    <div  @click="create" class="new">
 <!--      <Icon name="add"/>-->
-<!--      新增标签-->
-<!--    </div>-->
+    </div>
   </div>
 </template>
 
@@ -32,15 +31,15 @@ export default class Tags extends Vue {
     this.$emit('update:value', this.selectedTags)
   }
 
-  // create(){
-  //   const name = window.prompt('请输入标签名')
-  //   if(name === ''){
-  //     window.alert('标签名不能为空');
-  //   }else if(this.dataSource){
-  //       this.$emit('update:dataSource',
-  //       [...this.dataSource, name])
-  //   }
-  // }
+  create(){
+    const name = window.prompt('请输入标签名')
+    if(name === ''){
+      window.alert('标签名不能为空');
+    }else if(this.dataSource){
+        this.$emit('update:dataSource',
+        [...this.dataSource, name])
+    }
+  }
 };
 </script>
 
