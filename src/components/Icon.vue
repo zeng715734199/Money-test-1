4d87792f769fc3125236c76ab2fb7fa086c87d71
zeng715734199 <715734199@qq.com>
2021-09-09 17:16:15 +0800

    可以编辑和删除标签

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg class="icon">
+  <svg class="icon" @click="$emit('click', $event)">
     <!--suppress HtmlUnknownAttribute -->
     <use :xlink:href="'#'+name"/>
   </svg>
--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -8,6 +8,8 @@ type TagListModel = {
   data: Tag[]
   fetch: () => Tag[]
   create: (name: string) => 'success' | 'duplicated'
+  update: (id: string, name: string) => 'success' | 'not Found' | 'duplicated'
+  remove: (id: string) => boolean
   save: () => void
 }
 const tagListModel: TagListModel = {
@@ -17,11 +19,39 @@ const tagListModel: TagListModel = {
     return this.data;
   },
   create(name) {
-    const names = this.data.map(item => item.name)
-    if(names.indexOf(name) >= 0){return 'duplicated';}
+    const names = this.data.map(item => item.name);
+    if (names.indexOf(name) >= 0) {return 'duplicated';}
     this.data.push({id: name, name: name});
     this.save();
     return 'success';
+  },
+  update(id, name) {
+    const idList = this.data.map(item => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.data.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        return 'duplicated';
+      } else {
+        const tag = this.data.filter(item => item.id === id)[0];
+        tag.name = name;
+        this.save();
+        return 'success';
+      }
+    } else {
+      return 'not Found';
+    }
+  },
+  remove(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.data.splice(index, 1);
+    this.save();
+    return true;
   },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
