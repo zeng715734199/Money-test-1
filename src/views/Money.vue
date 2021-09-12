a572530ef0895a783d1409b6c874f8b4b2dde857
zeng715734199 <715734199@qq.com>
2021-09-12 18:39:03 +0800

    删除Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -13,7 +13,6 @@
 <script lang="ts">
 import Vue from 'vue';
 import Tabs from '@/components/Tabs.vue';
-import Types from '@/components/Money/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
@@ -21,7 +20,7 @@ import {Component} from 'vue-property-decorator';
 import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: {Tabs, FormItem, Types, Tags, NumberPad},
+  components: {Tabs, FormItem, Tags, NumberPad},
 })
 export default class Money extends Vue {
   get recordList() {
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -12,14 +12,13 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import Types from '@/components/Money/Types.vue';
 import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 import intervalList from '@/constants/intervalList';
 
 @Component({
-  components: {Tabs, Types},
+  components: {Tabs},
 })
 export default class Statistics extends Vue {
   type = '-';
